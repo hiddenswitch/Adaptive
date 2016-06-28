2cedc216a42bd74833d6f684944010097b1d5f86
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-06-27 21:07:53 -0700

    Try the fastest possible TimeClock implementation

--- TimeClockHelper.cs
+++ TimeClockHelper.cs
@@ -16,6 +16,7 @@ namespace HiddenSwitch.Multiplayer
 		internal int framesPerSecond = 60;
 		internal bool endOfFrame;
 		internal TimeClock timeClock;
+		internal bool timerCoroutineRunning = false;
 		protected float m_lastTime;
 
 		public long ElapsedTicks {
@@ -32,35 +33,58 @@ namespace HiddenSwitch.Multiplayer
 
 		void Start ()
 		{
-			StartCoroutine (Timer ());
+			StartCoroutine (TimerCoroutine ());
 		}
 
-		IEnumerator Timer ()
+		void Update ()
 		{
-			while (true) {
-				m_lastTime = Time.time;
-				while (running) {
-					if (endOfFrame) {
-						yield return new WaitForEndOfFrame ();
-					} else {
-						if (framesPerSecond <= 0) {
-							running = false;
-							break;
-						}
+			if (endOfFrame
+			    && running) {
+				var currentTime = Time.time;
+				ElapsedTicks = (long)((currentTime - m_lastTime) * 10e10);
+				m_lastTime = currentTime;
 
-						yield return new WaitForSeconds (1.0f / (float)framesPerSecond);
-					}
-
-					var currentTime = Time.time;
-					ElapsedTicks = (long)((currentTime - m_lastTime) * 10e10);
-					m_lastTime = currentTime;
-
-					if (timeClock != null) {
-						timeClock.HelperTick ();
-					}
+				if (timeClock != null) {
+					timeClock.HelperTick ();
 				}
-				while (!running) {
-					yield return null;
+			} else if (!endOfFrame
+			           && running
+			           && !timerCoroutineRunning) {
+				StartCoroutine (TimerCoroutine ());
+				timerCoroutineRunning = true;
+			}
+		}
+
+		IEnumerator TimerCoroutine ()
+		{
+			timerCoroutineRunning = true;
+			while (true) {
+				if (!running) {
+					timerCoroutineRunning = false;
+					StopAllCoroutines ();
+					yield break;
+
+				}
+				m_lastTime = Time.time;
+				if (endOfFrame) {
+					timerCoroutineRunning = false;
+					StopAllCoroutines ();
+					yield break;
+				} else {
+					if (framesPerSecond <= 0) {
+						running = false;
+						break;
+					}
+
+					yield return new WaitForSeconds (1.0f / (float)framesPerSecond);
+				}
+
+				var currentTime = Time.time;
+				ElapsedTicks = (long)((currentTime - m_lastTime) * 10e10);
+				m_lastTime = currentTime;
+
+				if (timeClock != null) {
+					timeClock.HelperTick ();
 				}
 			}
 		}
